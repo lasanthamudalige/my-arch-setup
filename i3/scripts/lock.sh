63699683ebbf3b14f5c947e37af87e8ab8e0f9ab
Lasantha Mudalige <91461938+lasanthamudalige@users.noreply.github.com>
2023-03-13 19:46:43 +0530

    Delete i3 directory

--- i3/scripts/lock.sh
+++
@@ -1,9 +0,0 @@
-img=/tmp/i3lock.png
-
-# Get a screenshot and save it image location
-scrot -o $img
-# Scale the image
-convert $img -scale 10% -scale 1000% $img
-
-# pass image as -i and -f to show times password was wrong  
-i3lock -i $img -f
